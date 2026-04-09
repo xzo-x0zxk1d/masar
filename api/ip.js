b00fa1233a308761d6fe0b045adc173c2b06b58d
xzo-x0zxk1d <fisaltd3512@gmail.com>
2026-04-09 22:40:11 +0300

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,25 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-
-const dataFile = path.join(process.cwd(), 'visitors.json');
+const dataFile = path.join('/tmp', 'visitors.json');
 
 module.exports = async (req, res) => {
     let visitors = {};
-    try {
-        if (fs.existsSync(dataFile)) {
-            visitors = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
-        }
-    } catch (e) {}
-
+    try { if (fs.existsSync(dataFile)) visitors = JSON.parse(fs.readFileSync(dataFile, 'utf8')); } catch(e){}
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const key = ip.split(',')[0].trim();
     let count = Object.keys(visitors).length;
-
     if (!visitors[key]) {
         visitors[key] = new Date().toISOString();
         count++;
         fs.writeFileSync(dataFile, JSON.stringify(visitors));
     }
-
     res.status(200).json({ count });
 };
